b54eafbc8f541de37574999d357bc608bfa260bf
Leonardo Lima <leolima.str@gmail.com>
2022-10-07 17:13:25 -0300

    test: creating tests for character list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useCallback, useEffect, useState } from "react";
-import "./App.css";
+import "./styles/main.sass";
 import { useDispatch, useSelector } from "react-redux";
 import { characterList } from "./redux/characterAction";
 
 function App() {
-  const [searchedTerm, setSearchedTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
   const dispatch = useDispatch();
   const characters = useSelector((state) => state.characterData);
 
@@ -12,22 +12,22 @@ function App() {
     dispatch(characterList());
   }, []);
 
-  const changeSearchedTerm = useCallback(
-    (input) => setSearchedTerm(input?.target?.value),
+  const changeSearchTerm = useCallback(
+    (input) => setSearchTerm(input?.target?.value),
     []
   );
 
   return (
     <div className="App">
       <img src="/logo.svg" alt="Rick and Morty" />
-      <input type="text" onChange={changeSearchedTerm} placeholder="Buscar" />
+      <input type="text" onChange={changeSearchTerm} placeholder="Buscar" />
       {characters
         .filter((c) =>
-          c?.name?.toLowerCase().includes(searchedTerm.toLowerCase())
+          c?.name?.toLowerCase().includes(searchTerm.toLowerCase())
         )
         .map((c) => (
-          <div key={c.name}>
-            <div>{c.name}</div>
+          <div className="card" key={c.name}>
+            <h2>{c.name}</h2>
             <div>{c.gender}</div>
             <div>{c.status}</div>
             <div>{c.species}</div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import { screen } from "@testing-library/react";
 import { customRender } from "./testUtils";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test("renders learn react link", () => {
+test("should render without problem", () => {
   customRender(<App />);
   const logo = screen.getByAltText(/Rick and Morty/i);
   expect(logo).toBeInTheDocument();
+});
+
+test("Should render List of characters", async () => {
+  const { findAllByRole } = customRender(<App />);
+
+  const characters = await findAllByRole("heading", { level: 2 });
+  expect(characters).toHaveLength(20);
+});
+
+test("Should filter by search term", async () => {
+  const { getByPlaceholderText, findAllByRole } = customRender(<App />);
+
+  const input = await getByPlaceholderText("Buscar");
+
+  userEvent.type(input, "Bet");
+
+  const characters = await findAllByRole("heading", { level: 2 });
+
+  expect(characters).toHaveLength(1);
 });
