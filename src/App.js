706b42dbeea936181c72c6ac1ee2bd36eaff6220
Leonardo Lima <leolima.str@gmail.com>
2022-10-07 15:18:45 -0300

    feat: applying search by name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { characterList } from "./redux/characterAction";
 
 function App() {
+  const [searchedTerm, setSearchedTerm] = useState("");
   const dispatch = useDispatch();
   const characters = useSelector((state) => state.characterData);
 
@@ -11,16 +12,24 @@ function App() {
     dispatch(characterList());
   }, []);
 
+  const changeSearchedTerm = useCallback(
+    (input) => setSearchedTerm(input?.target?.value),
+    []
+  );
+
   return (
     <div className="App">
-      {characters.map((c) => (
-        <div key={c.name}>
-          <div>{c.name}</div>
-          <div>{c.gender}</div>
-          <div>{c.status}</div>
-          <div>{c.species}</div>
-        </div>
-      ))}
+      <input type="text" onChange={changeSearchedTerm} placeholder="Buscar" />
+      {characters
+        .filter((c) => c?.name?.includes(searchedTerm))
+        .map((c) => (
+          <div key={c.name}>
+            <div>{c.name}</div>
+            <div>{c.gender}</div>
+            <div>{c.status}</div>
+            <div>{c.species}</div>
+          </div>
+        ))}
     </div>
   );
 }
